4185687c034b715a4c2fc1648939fd42f0526ceb
hyunjun9788 <namhyunjun9788@gmail.com>
2023-07-08 19:50:28 +0900

    feat: 여러개의 input값 하나의 state로 관리

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Input from "./components/Input";
 import React, {useState} from "react";
@@ -6,38 +6,75 @@ import Button from "./components/Button";
 
 function App() {
 
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [rePassword, setRePassword] = useState('')
-    const [name, setName] = useState('')
-    const [age, setAge] = useState('')
+    const [input, setInput] = useState({
+        email:'',
+        password:'',
+        rePassword:'',
+        name:'',
+        age:''
+    })
+    // const [email, setEmail] = useState('')
+    // const [password, setPassword] = useState('')
+    // const [rePassword, setRePassword] = useState('')
+    // const [name, setName] = useState('')
+    // const [age, setAge] = useState('')
 
 
     const emailInputHandler=(e)=>{
-        setEmail(e.target.value)
+        setInput((prevState)=>{
+            return{
+                ...prevState,
+                email:e.target.value
+            }
+        })
+        // setEmail(e.target.value)
     }
     const passwordInputHandler=(e)=>{
-        setPassword(e.target.value)
+        setInput((prevState)=>{
+            return{
+                ...prevState,
+                password:e.target.value
+            }
+        })
+        // setPassword(e.target.value)
     }
     const rePasswordInputHandler=(e)=>{
-        setRePassword(e.target.value)
+        setInput((prevState)=>{
+            return{
+                ...prevState,
+                rePassword:e.target.value
+            }
+        })
+        // setRePassword(e.target.value)
     }
     const nameInputHandler=(e)=>{
-        setName(e.target.value)
+        setInput((prevState)=>{
+            return{
+                ...prevState,
+                name:e.target.value
+            }
+        })
+        // setName(e.target.value)
     }
     const ageInputHandler=(e)=>{
-        setAge(e.target.value)
+        setInput((prevState)=>{
+            return{
+                ...prevState,
+                age:e.target.value
+            }
+        })
+        // setAge(e.target.value)
     }
 
     const formSubmitHandler = (e)=>{
         e.preventDefault()
 
         const fields = [
-            {value:email, message:"'@'를 포함하여 이메일을 작성해주세요."},
-            {value:password, message:'비밀번호를 입력해주세요.'},
-            { value: rePassword, message: '비밀번호 재확인을 입력해주세요.' },
-            { value: name, message: '이름을 입력해주세요.' },
-            { value: age, message: '나이를 입력해주세요.' },
+            {value:input.email, message:"'@'를 포함하여 이메일을 작성해주세요."},
+            {value:input.password, message:'비밀번호를 입력해주세요.'},
+            { value: input.rePassword, message: '비밀번호 재확인을 입력해주세요.' },
+            { value: input.name, message: '이름을 입력해주세요.' },
+            { value: input.age, message: '나이를 입력해주세요.' },
         ]
 
         for(const field of fields){
@@ -53,19 +90,19 @@ function App() {
                 <h1>회원가입</h1>
 
                 <label>이메일<span className='required'>필수<span className='star'>＊</span></span></label>
-                <Input type='text' id='email' value={email} onChange={emailInputHandler} placeholder='이메일을 입력하세요' required/>
+                <Input type='text' id='email' value={input.email} onChange={emailInputHandler} placeholder='이메일을 입력하세요' required/>
 
                 <label>비밀번호<span className='required'>필수<span className='star'>＊</span></span></label>
-                <Input type='password' id='password' value={password} onChange={passwordInputHandler} placeholder='비밀번호를 입력하세요' required/>
+                <Input type='password' id='password' value={input.password} onChange={passwordInputHandler} placeholder='비밀번호를 입력하세요' required/>
 
                 <label>비밀번호 재확인<span className='required'>필수<span className='star'>＊</span></span></label>
-                <Input type='password' id='repassword' value={rePassword} onChange={rePasswordInputHandler} placeholder='비밀번호를 입력하세요' required/>
+                <Input type='password' id='repassword' value={input.rePassword} onChange={rePasswordInputHandler} placeholder='비밀번호를 입력하세요' required/>
 
                 <label>이름</label>
-                <Input type='text' id='name' value={name} onChange={nameInputHandler}  placeholder='이름을 입력하세요' />
+                <Input type='text' id='name' value={input.name} onChange={nameInputHandler}  placeholder='이름을 입력하세요' />
 
                 <label>나이</label>
-                <Input type='number' id='age' value={age} onChange={ageInputHandler} placeholder='나이를 입력하세요'/>
+                <Input type='number' id='age' value={input.age} onChange={ageInputHandler} placeholder='나이를 입력하세요'/>
 
                 <Button type='submit' onClick={formSubmitHandler} className='signup-bt'>가입하기</Button>
             </form>
