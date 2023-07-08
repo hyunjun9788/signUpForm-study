1a927bb4ab7cf093b3aa72792e3011998acfbcd8
hyunjun9788 <namhyunjun9788@gmail.com>
2023-07-08 19:14:10 +0900

    feat: Input 컴포넌트 분리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Input from "./components/Input";
 
 function App() {
     return (
@@ -8,19 +9,19 @@ function App() {
                 <h1>회원가입</h1>
 
                 <label>이메일<span className='required'>필수<span className='star'>＊</span></span></label>
-                <input type='text' id='email' placeholder='이메일을 입력하세요' required/>
+                <Input type='text' id='email' placeholder='이메일을 입력하세요' required/>
 
                 <label>비밀번호<span className='required'>필수<span className='star'>＊</span></span></label>
-                <input type='password' id='password' placeholder='비밀번호를 입력하세요' required/>
+                <Input type='password' id='password' placeholder='비밀번호를 입력하세요' required/>
 
                 <label>비밀번호 재확인<span className='required'>필수<span className='star'>＊</span></span></label>
-                <input type='password' id='repassword' placeholder='비밀번호를 입력하세요' required/>
+                <Input type='password' id='repassword' placeholder='비밀번호를 입력하세요' required/>
 
                 <label>이름</label>
-                <input type='text' id='name' placeholder='이름을 입력하세요' />
+                <Input type='text' id='name' placeholder='이름을 입력하세요' />
 
                 <label>나이</label>
-                <input type='text' id='age' placeholder='나이를 입력하세요'/>
+                <Input type='number' id='age' placeholder='나이를 입력하세요'/>
 
                 <button type='button' className='signup-bt'>가입하기</button>
             </form>
---
+++ src/components/Input.js
@@ -0,0 +1,7 @@
+function Input({type,id,placeholder,required}){
+    return (
+        <input type={type} id={id} placeholder={placeholder} required={required}/>
+    )
+}
+
+export default Input
