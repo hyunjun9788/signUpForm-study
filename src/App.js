0a7b91fed3d2aac385f1010517c523ea4219beb8
hyunjun9788 <namhyunjun9788@gmail.com>
2023-07-08 19:00:08 +0900

    feat: form 구성 완료

--- src/App.js
+++ src/App.js
@@ -2,24 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='container'>
+            <form className='signup-form'>
+                <h1>회원가입</h1>
+
+                <label>이메일<span className='required'>필수<span className='star'>＊</span></span></label>
+                <input type='text' id='email' placeholder='이메일을 입력하세요' required/>
+
+                <label>비밀번호<span className='required'>필수<span className='star'>＊</span></span></label>
+                <input type='password' id='password' placeholder='비밀번호를 입력하세요' required/>
+
+                <label>비밀번호 재확인<span className='required'>필수<span className='star'>＊</span></span></label>
+                <input type='password' id='repassword' placeholder='비밀번호를 입력하세요' required/>
+
+                <label>이름</label>
+                <input type='text' id='name' placeholder='이름을 입력하세요' />
+
+                <label>나이</label>
+                <input type='text' id='age' placeholder='나이를 입력하세요'/>
+
+                <button type='button' className='signup-bt'>가입하기</button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
