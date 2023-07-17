a7d70527fbd29a838f87581c780791f4cc8738d2
hyunjun9788 <namhyunjun9788@gmail.com>
2023-07-17 15:42:04 +0900

    feat: 이름 출력 기능 추가

--- src/App.js
+++ src/App.js
@@ -36,7 +36,8 @@ function App() {
             alert('비밀번호와 비밀번호 재확인에 입력한 값이 다릅니다.')
             return
         }
-        alert('남현준님 환영합니다!')
+
+        alert(`${name}님 환영합니다!`)
 
 
         setEmail('')
@@ -50,13 +51,11 @@ function App() {
         <div className='box'>
             <Header/>
             <form onSubmit={submitHandler}>
-                <InputContainer labelName='이메일' onChangeEmail={changeEmailHandler} isRequired={true} inputType='email' placeholder={'이메일을 입력해주세요'}/>
-                <InputContainer labelName='비밀번호' onChangePassword={changePasswordHandler} isRequired={true} inputType='password' placeholder={'비밀번호를 입력해주세요'}/>
-                <InputContainer labelName='비밀번호 재확인' onChangePassword={changeRePasswordHandler} isRequired={true} inputType='password' placeholder={'비밀번호를 다시 입력해주세요'}/>
-                <InputContainer labelName='이름' onChangeName={changeNameHandler}  inputType='text' placeholder={'이름을 입력해주세요'}/>
-                <InputContainer labelName='나이' onChangeAge={changeAgeHandler}  inputType='number' placeholder={'나이를 입력해주세요'}/>
-
-
+                <InputContainer labelName='이메일' onChange={changeEmailHandler} isRequired={true} inputType='email' placeholder={'이메일을 입력해주세요'}/>
+                <InputContainer labelName='비밀번호' onChange={changePasswordHandler} isRequired={true} inputType='password' placeholder={'비밀번호를 입력해주세요'}/>
+                <InputContainer labelName='비밀번호 재확인' onChange={changeRePasswordHandler} isRequired={true} inputType='password' placeholder={'비밀번호를 다시 입력해주세요'}/>
+                <InputContainer labelName='이름' onChange={changeNameHandler}  inputType='text' placeholder={'이름을 입력해주세요'}/>
+                <InputContainer labelName='나이' onChange={changeAgeHandler}  inputType='number' placeholder={'나이를 입력해주세요'}/>
                 <Button type='submit'/>
             </form>
         </div>
--- src/components/InputContainer.js
+++ src/components/InputContainer.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const InputContainer = ({labelName, isRequired, inputType,placeholder}) => {
+const InputContainer = ({labelName, isRequired, inputType,placeholder, onChange}) => {
     return (
             <div className='input-container'>
                 <div>
                     <label>{labelName}</label>
                     {isRequired && <span className='star'>필수<span>*</span></span>}
                 </div>
-                <input type={inputType} placeholder={placeholder} required={isRequired}/>
+                <input onChange={onChange} type={inputType} placeholder={placeholder} required={isRequired}/>
             </div>
 
     );
